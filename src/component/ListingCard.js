9ebab929824d72ed94797ba49b7356533d65831b
WaffleWaldo <oswaldoortiz0519@gmail.com>
2020-12-01 10:58:18 -0500

    Render Instrument cards

---
+++ src/component/ListingCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+
+export default class ListingCard extends React.Component {
+    render() {
+        return(
+            <Card>
+            <div>
+                <div className="image">
+                    <img src="" alt="img" />
+                </div>
+                <div className="content">
+                    <div className="header">{this.props.instrumentType}</div>
+                </div>
+                <div className="extra content">
+                    <span>
+                        <div className="history">Background: {this.props.history}</div>
+                        <div className="weight">Weight: {this.props.weight}</div>
+                        <div className="age">Age: {this.props.age}</div>
+                        <div className="used?">{this.props.used ? "Used" : "New"}</div>
+                        <div className="price">Price: {this.props.price}</div>
+                    </span>
+                </div>
+                <br />
+            </div>
+            </Card>
+        )
+    }
+}
--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -2,27 +2,21 @@ import React from 'react'
 import NavBar from '../component/NavBar'
 import {Button, ButtonToolbar} from 'react-bootstrap'
 import InstrumentModal from '../component/InstrumentModal'
+import ListingContainer from './ListingContainer'
 // import ListingContainer from './container/ListingContainer'
 
 class MainContainer extends React.Component {
 
     state = {
-        listingArray: [],
         addModalShow: false
-    }
-
-    componentDidMount(){
-        fetch('http://localhost:3000/api/v1/listings/')
-            .then(response => response.json())
-            .then(listingData => this.setState({ listingArray: listingData}))
     }
 
     render() {
         let addModalClose = () => this.setState({ addModalShow: false })
-        console.log(this.state.listingArray)
         return (
             <div>
                 <NavBar />
+                <ListingContainer />
                     <Button variant='primary' onClick={() => this.setState({addModalShow: true})}>Add Listing</Button>
                     <InstrumentModal show={this.state.addModalShow} onHide={addModalClose} />
             </div>
